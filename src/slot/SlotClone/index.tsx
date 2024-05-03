52e11e2fc64e0d94c8ff3891709b712f3f90f985
ag502 <ag502@naver.com>
2024-05-04 04:49:00 +0900

    feat: slot-clone 컴포넌트 compose ref  추가

--- src/slot/SlotClone/index.tsx
+++ src/slot/SlotClone/index.tsx
@@ -3,9 +3,21 @@ import { Children, cloneElement, forwardRef, isValidElement } from "react";
 import { MergeProps } from "@/utils/services";
 
 import type { SlotProps } from "../Slot/Slot.types";
-import type { ComponentPropsWithRef, ForwardedRef, ReactElement } from "react";
+import type { ComponentPropsWithRef, ForwardedRef, MutableRefObject, ReactElement, Ref } from "react";
 
 const MAX_NUM_OF_SLOTTED_CHILDREN = 1;
+
+function setRef<T>(node: T, ref?: Ref<T>) {
+  if (typeof ref === "function") {
+    ref(node);
+  } else if (ref) {
+    (ref as MutableRefObject<T>).current = node;
+  }
+}
+
+function combineRefs<T>(...refs: (Ref<T> | undefined)[]) {
+  return (node: T) => refs.forEach((ref) => setRef(node, ref));
+}
 
 export const SlotClone = forwardRef((props: SlotProps, forwardedRef: ForwardedRef<HTMLElement>) => {
   const { children, ...slotProps } = props;
@@ -14,6 +26,7 @@ export const SlotClone = forwardRef((props: SlotProps, forwardedRef: ForwardedRe
   if (Children.count(children) > MAX_NUM_OF_SLOTTED_CHILDREN) {
     return Children.only(null);
   }
+
   // children 이 ReactElement 인지 검사
   else if (isValidElement(children)) {
     return cloneElement<ComponentPropsWithRef<"element">>(children as ReactElement, {
@@ -22,7 +35,8 @@ export const SlotClone = forwardRef((props: SlotProps, forwardedRef: ForwardedRe
         .mergeStyles()
         .mergeClassNames(slotProps.postProcessClassName)
         .build(),
-      ref: forwardedRef,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ref: forwardedRef ? combineRefs(forwardedRef, (children as any)?.ref) : (children as any)?.ref,
     });
   }
 
