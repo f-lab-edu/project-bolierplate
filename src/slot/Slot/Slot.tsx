2497acd36561c65912b03a6cf26f02dfb1596ead
ag502 <ag502@naver.com>
2024-05-06 17:54:37 +0900

    feat: slot 컴포넌트에 slottable 컴포넌트 적용 로직 추가

--- src/slot/Slot/Slot.tsx
+++ src/slot/Slot/Slot.tsx
@@ -1,12 +1,49 @@
-import { forwardRef } from "react";
+import { Children, cloneElement, forwardRef, isValidElement } from "react";
 
 import { SlotClone } from "../SlotClone/SlotClone";
+import { isSlottable } from "../Slottable/Slottable";
 
 import type { SlotProps } from "./Slot.types";
-import type { ForwardedRef } from "react";
+import type { SlottableProps } from "../Slottable/Slottable.type";
+import type { ForwardedRef, ReactElement } from "react";
+
+const MAX_NUM_OF_SLOTTABLE = 1;
+const MAX_NUM_OF_SLOTTABLE_CHILDREN = 1;
 
 export const Slot = forwardRef((props: SlotProps, forwardedRef: ForwardedRef<HTMLElement>) => {
   const { children, ...slotProps } = props;
+  const childrenArray = Children.toArray(children);
+
+  const slottableComp: ReactElement<SlottableProps>[] = childrenArray.filter(isSlottable);
+
+  if (slottableComp.length > MAX_NUM_OF_SLOTTABLE) {
+    throw new Error("Slottable 컴포넌트는 하나만 존재해야 합니다.");
+  }
+
+  if (slottableComp?.[0]) {
+    const firstSlottable = slottableComp[0];
+    const newContainer = firstSlottable.props.children;
+
+    if (Children.count(newContainer) > MAX_NUM_OF_SLOTTABLE_CHILDREN) {
+      throw new Error("Slottable 컴포넌트의 children props 에는 단일 ReactElement 만 전달할 수 있습니다.");
+    }
+
+    if (!isValidElement(newContainer)) return null;
+
+    const slottableCompNewChildren = childrenArray.map((child) => {
+      if (child === firstSlottable) {
+        return newContainer.props.children;
+      } else {
+        return child;
+      }
+    });
+
+    return (
+      <SlotClone {...slotProps} ref={forwardedRef}>
+        {cloneElement(newContainer, undefined, ...slottableCompNewChildren)}
+      </SlotClone>
+    );
+  }
 
   return (
     <SlotClone {...slotProps} ref={forwardedRef}>
