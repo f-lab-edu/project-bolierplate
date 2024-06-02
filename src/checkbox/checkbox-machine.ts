5db8cedd4f5d00588d69002807bf308c8148d54f
ag502 <ag502@naver.com>
2024-06-02 19:22:29 +0900

    feat: context property 초기화 로직 추가

--- src/checkbox/checkbox-machine.ts
+++ src/checkbox/checkbox-machine.ts
@@ -1,4 +1,4 @@
-import { setup } from "xstate";
+import { setup, assign } from "xstate";
 
 import type { CheckboxMachineInput, CheckboxMachineContext } from "./checkbox.types";
 
@@ -7,6 +7,8 @@ export const checkboxMachine = setup({
     context: {} as CheckboxMachineContext,
     input: {} as CheckboxMachineInput,
   },
+
+  actions: { initContext: assign(({ context }) => ({ ...context, isControlled: context.checked !== undefined })) },
 }).createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QGMAWZkGsBGB7AHgMQDCAEgKLEDSAQgPIAaAdAMrkAqA+sXQHLvkG7ANoAGALqJQAB1ywAlgBd5uAHZSQ+RAA4ArE20B2XQBoQAT0QBGAEwBfO2bQYcBEhWr1mbLj36CRK0kkEFkFZTUNLQQATismABYrBNEANhtTC0RdbRsmGLSbVN0Mo204hyd0LDwiMkpaRiZ66nIAETFgmTklFXUQ6N1DM0sEAGZDVKZi9OLSw3K0ypBnGrcWxuYAVV4N9s6NMN7IgezhrIQEwxjp3VmSnIWC1OXV1zqPTaYASV42jnIACUALK-ACCAgOISOEX6oEG51GNkMY1u6SG6RiYxiCV0YwcjhAqlwEDgGjetUOPVhUUQMRG1ishkShV0r2q7yYb0gVPCfVp4xsNxiNisRkyozGugS+UKc0eixehIpBCYAFdVNyILzjnDNIgxkL8qLxQzLkNZfd5or2S5akx5KpSYowAAnAC2joAhi6dTTToLhSbjGaMvFnhkCXYgA */
   id: "checkbox",
@@ -17,6 +19,8 @@ export const checkboxMachine = setup({
       hover: false,
     };
   },
+
+  entry: ["initContext"],
 
   initial: "unchecked",
 
