def541ce28b285d46b70c943e7fe18c43cfcdc26
ag502 <ag502@naver.com>
2024-06-09 15:27:31 +0900

    feat: avatar-machine 상태 추가

--- src/avatar/machine/avatar-machine.ts
+++ src/avatar/machine/avatar-machine.ts
@@ -1,5 +1,21 @@
 import { setup } from "xstate";
 
-export const avatarMachine = setup({}).createMachine({});
+export const avatarMachine = setup({}).createMachine({
+  id: "avatar",
+
+  initial: "loading",
+
+  states: {
+    loading: {
+      description: "초기상태 / 이미지 로드 중인 상태",
+    },
+    loaded: {
+      description: "이미지 로드 완료 상태",
+    },
+    error: {
+      description: "이미지 로드 오류 상태",
+    },
+  },
+});
 
 export const machine = avatarMachine;
