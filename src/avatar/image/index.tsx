d2f807e6537f3f3c972db075cc957589698d6cde
ag502 <ag502@naver.com>
2024-06-15 10:05:04 +0900

    feat: avatar-image 컴포넌트에 use-image-loading-status hook 추가

--- src/avatar/image/index.tsx
+++ src/avatar/image/index.tsx
@@ -5,16 +5,23 @@ import { mergeProps } from "@/utils/react";
 
 import { useAvatarContext } from "../root";
 
+import { useImageLoadingStatus } from "./use-image-loading-status";
+
 import type { AvatarImageProps } from "../avatar.types";
 import type { ForwardedRef } from "react";
 
 export const AvatarImage = forwardRef((props: AvatarImageProps, forwardedRef: ForwardedRef<HTMLImageElement>) => {
   const { asChild, ...avatarImageProps } = props;
+  const { src, srcSet, crossOrigin, referrerPolicy } = avatarImageProps;
+
   const Comp = asChild ? Slot : "img";
 
   const avatarContext = useAvatarContext();
+  const imageLoadingStatus = useImageLoadingStatus({ src, srcSet, crossOrigin, referrerPolicy });
 
-  return <Comp ref={forwardedRef} {...mergeProps(avatarImageProps, avatarContext?.api.imageProps)} />;
+  return imageLoadingStatus === "SUCCESS" ? (
+    <Comp ref={forwardedRef} {...mergeProps(avatarImageProps, avatarContext?.api.imageProps)} />
+  ) : null;
 });
 
 AvatarImage.displayName = "Avatar.Image";
