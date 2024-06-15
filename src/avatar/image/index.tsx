5a8d79d2022e1b8080dcddc0fe12a1bfe3354489
ag502 <ag502@naver.com>
2024-06-15 15:47:16 +0900

    feat: avatar-image 컴포넌트에 상태 변경 callback 함수 props 추가

--- src/avatar/image/index.tsx
+++ src/avatar/image/index.tsx
@@ -1,17 +1,17 @@
-import { forwardRef } from "react";
+import { forwardRef, useLayoutEffect } from "react";
 
 import { Slot } from "@/slot";
-import { mergeProps } from "@/utils/react";
+import { useCallbackRef } from "@/utils/react";
 
 import { useAvatarContext } from "../root";
 
 import { useImageLoadingStatus } from "./use-image-loading-status";
 
-import type { AvatarImageProps } from "../avatar.types";
+import type { AvatarImageProps, ImageLoadingStatus } from "../avatar.types";
 import type { ForwardedRef } from "react";
 
 export const AvatarImage = forwardRef((props: AvatarImageProps, forwardedRef: ForwardedRef<HTMLImageElement>) => {
-  const { asChild, ...avatarImageProps } = props;
+  const { asChild, onImageLoadingStatusChange, ...avatarImageProps } = props;
   const { src, srcSet, crossOrigin, referrerPolicy } = avatarImageProps;
 
   const Comp = asChild ? Slot : "img";
@@ -19,9 +19,18 @@ export const AvatarImage = forwardRef((props: AvatarImageProps, forwardedRef: Fo
   const avatarContext = useAvatarContext();
   const imageLoadingStatus = useImageLoadingStatus({ src, srcSet, crossOrigin, referrerPolicy });
 
-  return imageLoadingStatus === "SUCCESS" ? (
-    <Comp ref={forwardedRef} {...mergeProps(avatarImageProps, avatarContext?.api.imageProps)} />
-  ) : null;
+  const handleImageLoadingStatus = useCallbackRef((status: ImageLoadingStatus) => {
+    onImageLoadingStatusChange?.(status);
+    avatarContext?.onImageLoadingStatusChange(status);
+  });
+
+  useLayoutEffect(() => {
+    if (imageLoadingStatus !== "IDLE") {
+      handleImageLoadingStatus.current?.(imageLoadingStatus);
+    }
+  }, [imageLoadingStatus, handleImageLoadingStatus]);
+
+  return imageLoadingStatus === "SUCCESS" ? <Comp ref={forwardedRef} {...avatarImageProps} /> : null;
 });
 
 AvatarImage.displayName = "Avatar.Image";
