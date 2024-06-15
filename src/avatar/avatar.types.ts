b63889e8f67634f0ea97032aefca4a2568aa631d
ag502 <ag502@naver.com>
2024-06-15 16:03:12 +0900

    fix: avatar-machine 관련 코드 삭제

--- src/avatar/avatar.types.ts
+++ src/avatar/avatar.types.ts
@@ -1,26 +1,11 @@
-import type { avatarConnector } from "./machine/avatar-connector";
-import type { avatarMachine } from "./machine/avatar-machine";
 import type { Dispatch, ReactNode } from "react";
-import type { EventFromLogic, SnapshotFrom } from "xstate";
-
-export type AvatarMachine = typeof avatarMachine;
-export type AvatarMachineState = SnapshotFrom<AvatarMachine>;
-export type AvatarMachineSend = (event: EventFromLogic<AvatarMachine>) => void;
-
-export type AvatarConnectorApi = ReturnType<typeof avatarConnector>;
-
-export interface AvatarMachineInput {}
-
-export interface AvatarMachineContext extends AvatarMachineInput {}
-
-export type AvatarMachineEvent = { type: "AVATAR.LOADED" } | { type: "AVATAR.ERROR" };
 
 export type AvatarContext = {
   imageLoadingStatus: ImageLoadingStatus;
   onImageLoadingStatusChange: Dispatch<ImageLoadingStatus>;
 };
 
-export interface AvatarProps extends ComponentPropsWithoutRefWithAsChild<"span">, AvatarMachineInput {}
+export interface AvatarProps extends ComponentPropsWithoutRefWithAsChild<"span"> {}
 
 export interface AvatarImageProps extends ComponentPropsWithoutRefWithAsChild<"img"> {
   onImageLoadingStatusChange?: (status: ImageLoadingStatus) => void;
