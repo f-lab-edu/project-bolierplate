124de62a889e6ec538dc3f47b3e897dcddf9c982
ag502 <ag502@naver.com>
2024-06-21 19:20:46 +0900

    feat: useSwitch 및 get-switch-extra-props 적용

--- src/switch/root/index.tsx
+++ src/switch/root/index.tsx
@@ -1,16 +1,26 @@
 import { forwardRef } from "react";
 
 import { Slot } from "@/slot";
+import { mergeProps } from "@/utils/react";
+
+import { getSwitchExtraProps } from "../get-switch-extra-props";
+
+import { useSwitch } from "./use-switch";
 
 import type { SwitchRootProps } from "../switch.types";
 import type { ForwardedRef } from "react";
 
 export const Switch = forwardRef((props: SwitchRootProps, forwardedRef: ForwardedRef<HTMLButtonElement>) => {
-  const { asChild, ...switchProps } = props;
+  const { asChild, ...switchRootProps } = props;
 
   const Comp = asChild ? Slot : "button";
 
-  return <Comp ref={forwardedRef} {...switchProps} />;
+  const { checked, ...computedRootProps } = useSwitch(switchRootProps);
+  const { disabled } = computedRootProps;
+
+  const extraProps = getSwitchExtraProps({ checked, disabled });
+
+  return <Comp ref={forwardedRef} {...mergeProps(extraProps.rootProps, computedRootProps)} />;
 });
 
 Switch.displayName = "Switch.Root";
